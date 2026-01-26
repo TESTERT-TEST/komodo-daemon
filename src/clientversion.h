b0f6b335efa26cbe94669100cd3f1989f17422fa
DeckerSU <deckersu@protonmail.com>
2026-01-26 18:25:38 +0100

    Update versioning and copyright year in configuration files (#680)
    - Bump client version build number to 51
    - Update copyright year to 2026
    - Modify repository URL in AC_INIT for issue tracking
    - Adjust KOMODO_VERSION to include a suffix for the new build

--- src/clientversion.h
+++ src/clientversion.h
@@ -33,7 +33,7 @@
 #define CLIENT_VERSION_MAJOR 0
 #define CLIENT_VERSION_MINOR 9
 #define CLIENT_VERSION_REVISION 2
-#define CLIENT_VERSION_BUILD 0
+#define CLIENT_VERSION_BUILD 51
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
